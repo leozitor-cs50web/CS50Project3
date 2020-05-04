cc7e67dc8a724c2cc348e8466dec7e9e89baf0d5
Leozitor Floro de Souza <leozitor@gmail.com>
2020-05-03 22:06:11 -0300

    first commit, models created, maybe need to verify.

---
+++ orders/models.py
@@ -0,0 +1,93 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+
+class RegularPizza(models.Model):
+    name = models.CharField(max_length=64)
+    small = models.DecimalField(max_digits=4, decimal_places=2)
+    large = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} - {self.small} -{self.large}"
+
+
+class ScilianPizza(models.Model):
+    name = models.CharField(max_length=64)
+    small = models.DecimalField(max_digits=4, decimal_places=2)
+    large = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} - {self.small} -{self.large}"
+
+
+class Sub(models.Model):
+    name = models.CharField(max_length=64)
+    small = models.DecimalField(max_digits=4, decimal_places=2)
+    large = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} - {self.small} -{self.large}"
+
+
+class Pasta(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} - {self.price}"
+
+
+class Salad(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} - {self.price}"
+
+
+class DinnerPlatter(models.Model):
+    name = models.CharField(max_length=64)
+    small = models.DecimalField(max_digits=4, decimal_places=2)
+    large = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} - {self.small} -{self.large}"
+
+
+class Topping(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class User_order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_number = models.IntegerField()
+    topping_allowance = models.IntegerField(default=0)
+    status = models.CharField(max_length=64, default='initiated')
+
+    def __str__(self):
+        return f"{self.user} - {self.order_number} - {self.status} Topping_allowance: {self.topping_allowance}"
+
+
+class Order2(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    number = models.IntegerField()
+    category = models.CharField(max_length=64, null=True)
+    name = models.CharField(max_length=64)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} - ${self.price} "
+
+
+class Order_counter(models.Model):
+    counter = models.IntegerField()
+
+    def __str__(self):
+        return f"Order no: {self.counter}  "
