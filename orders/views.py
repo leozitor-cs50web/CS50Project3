fb41a9df1369ca252e2b94847a0c98bef6232672
Leozitor Floro de Souza <leozitor@gmail.com>
2020-05-10 03:12:13 -0400

    loggin page almost ready, need to ajust sign up page

--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -22,8 +23,35 @@ def signin(request):
     return render(request, "orders/signin.html")
 
 
+def login_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "orders/signin.html", {"message": "Invalid credentials."})
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, "orders/index.html", {"message": "Logged out."})
+
+
 def signup(request):
     return render(request, "orders/signup.html")
+
+
+def createUser(request):
+    userName = request.POST["userName"]
+    firstName = request.POST["firstName"]
+    lastName = request.POST["lastName"]
+    email = request.POST["email"]
+    password = request.POST["password"]
+    confPassword = request.POST["confPassword"]
+    user = User.objects.create_user(userName, email, password)
+
 
 
 def contact(request):
