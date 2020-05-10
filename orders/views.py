13881fe6e48c0cc9153602055b588b34fbf4b2ce
Leozitor Floro de Souza <leozitor@gmail.com>
2020-05-10 02:19:08 -0400

    logged in layout and logged out layout ready

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
 from django.urls import reverse
@@ -5,7 +6,16 @@ from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    return render(request, "orders/index.html")
+    print("Usuario Autenticado: {}".format(request.user.is_authenticated))
+    if not request.user.is_authenticated:
+        logged = False
+        return render(request, "orders/index.html", {"message": None, "logged": logged})
+    logged = True
+    context = {
+        "user": request.user,
+        "logged": logged
+    }
+    return render(request, "orders/index.html", context)
 
 
 def signin(request):
